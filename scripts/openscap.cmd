1992ab8a13c4edca048e770f09bca38240427bbf
Wee Yee Hong <69478469+w-yeehong@users.noreply.github.com>
2024-11-04 10:46:56 +0800

    Fix download of OVAL definition for RHEL 8 (#4)
    * fix: Include flag to follow redirects when downloading OVAL definition for RHEL 8

--- scripts/openscap.cmd
+++ scripts/openscap.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo =====Start download for openscap=======
 
-curl --ssl-no-revoke https://access.redhat.com/security/data/oval/v2/RHEL8/rhel-8.oval.xml.bz2 -o %OPENSCAP_DIR%\rhel-8.oval.xml.bz2
+curl --ssl-no-revoke -L https://access.redhat.com/security/data/oval/v2/RHEL8/rhel-8.oval.xml.bz2 -o %OPENSCAP_DIR%\rhel-8.oval.xml.bz2
 
 :completed
 echo =====Complete download for openscap=======
